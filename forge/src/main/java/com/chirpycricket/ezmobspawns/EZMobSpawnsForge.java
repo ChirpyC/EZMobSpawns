9777b3fa2df9b8a68b536116d0357cdeef9b3a7f
ChirpyC <chirpycricket@yahoo.com>
2024-01-24 13:01:09 -0500

    Committed 1.18.2 port.

---
+++ forge/src/main/java/com/chirpycricket/ezmobspawns/EZMobSpawnsForge.java
@@ -0,0 +1,28 @@
+package com.chirpycricket.ezmobspawns;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class EZMobSpawnsForge {
+    
+    public EZMobSpawnsForge() {
+        // Use Forge to bootstrap the Common mod.
+        CommonClass.init();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void serverTickEvent(TickEvent.WorldTickEvent e) {
+//        for(ServerLevel level : e.getServer().getAllLevels()){
+//        for(ServerLevel level : e.world.getServer().getAllLevels()){
+//            SpawnTicker.onTick(level);
+//        }
+        if(!e.world.isClientSide){
+            SpawnTicker.onTick((ServerLevel)e.world);
+        }
+    }
+}
